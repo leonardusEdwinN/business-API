f07b7b361e4f9ce1ffa5a3f850acfc957773b490
Edwin <osproject78@gmail.com>
2022-01-21 10:12:59 +0700

    Create Initial Project

---
+++ business-app/business-app/Features/ListBusiness/Controller/ListBusinessViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ListBusinessViewController.swift
+//  business-app
+//
+//  Created by Edwin Niwarlangga on 21/01/22.
+//
+
+import Foundation
+import UIKit
+
+class ListBusinessViewController : UIViewController{
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
